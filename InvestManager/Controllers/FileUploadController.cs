f8e85e6a1226e4171cef1ab2f253ce09779322b2
Ruan <ruangbarros@gmail.com>
2021-01-21 23:22:53 -0300

    Creating FileUpload view to reado PDF files

---
+++ InvestManager/Controllers/FileUploadController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using InvestManager.Models;
+
+namespace InvestManager.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpPost]
+        public IActionResult ReadFile()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
