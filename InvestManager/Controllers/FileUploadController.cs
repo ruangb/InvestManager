57e54136a171f86e3fab64b739941fbdb50139a2
Ruan <ruangbarros@gmail.com>
2021-01-26 23:12:31 -0300

    Implementing FileUpload

--- InvestManager/Controllers/FileUploadController.cs
+++ InvestManager/Controllers/FileUploadController.cs
@@ -1,20 +1,84 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using InvestManager.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using iText;
+using System;
+using iText.Kernel.Pdf;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace InvestManager.Controllers
 {
     public class FileUploadController : Controller
     {
-        [HttpPost]
-        public IActionResult ReadFile()
+        //Define uma instância de IHostingEnvironment
+        IHostingEnvironment _appEnvironment;
+        //Injeta a instância no construtor para poder usar os recursos
+        public FileUploadController(IHostingEnvironment env)
         {
-            return View();
+            _appEnvironment = env;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        //método para enviar os arquivos usando a interface IFormFile
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> archives)
+        {
+            long tamanhoArquivos = archives.Sum(f => f.Length);
+            // caminho completo do arquivo na localização temporária
+            var caminhoArquivo = Path.GetTempFileName();
+
+            // processa os arquivo enviados
+            //percorre a lista de arquivos selecionados
+            foreach (var arquivo in archives)
+            {
+                //verifica se existem arquivos 
+                if (arquivo == null || arquivo.Length == 0)
+                {
+                    //retorna a viewdata com erro
+                    ViewData["Erro"] = "Error: Arquivo(s) não selecionado(s)";
+                    return View(ViewData);
+                }
+                // < define a pasta onde vamos salvar os arquivos >
+                string pasta = "Arquivos_Usuario";
+                // Define um nome para o arquivo enviado incluindo o sufixo obtido de milesegundos
+                string nomeArquivo = "Usuario_arquivo_" + DateTime.Now.Millisecond.ToString();
+                //verifica qual o tipo de arquivo : jpg, gif, png, pdf ou tmp
+                if (arquivo.FileName.Contains(".jpg"))
+                    nomeArquivo += ".jpg";
+                else if (arquivo.FileName.Contains(".gif"))
+                    nomeArquivo += ".gif";
+                else if (arquivo.FileName.Contains(".png"))
+                    nomeArquivo += ".png";
+                else if (arquivo.FileName.Contains(".pdf"))
+                    nomeArquivo += ".pdf";
+                else
+                    nomeArquivo += ".tmp";
+                //< obtém o caminho físico da pasta wwwroot >
+                string caminho_WebRoot = _appEnvironment.WebRootPath;
+                // monta o caminho onde vamos salvar o arquivo : 
+                // ~\wwwroot\Arquivos\Arquivos_Usuario\Recebidos
+                string caminhoDestinoArquivo = caminho_WebRoot + "\\Arquivos\\" + pasta + "\\";
+                // incluir a pasta Recebidos e o nome do arquivo enviado : 
+                // ~\wwwroot\Arquivos\Arquivos_Usuario\Recebidos\
+                string caminhoDestinoArquivoOriginal = caminhoDestinoArquivo + "\\Recebidos\\" + nomeArquivo;
+                //copia o arquivo para o local de destino original
+                using (var stream = new FileStream(caminhoDestinoArquivoOriginal, FileMode.Create))
+                {
+                    await arquivo.CopyToAsync(stream);
+                }
+            }
+            //monta a ViewData que será exibida na view como resultado do envio 
+            ViewData["Resultado"] = $"{archives.Count} arquivos foram enviados ao servidor, " +
+             $"com tamanho total de : {tamanhoArquivos} bytes";
+            //retorna a viewdata
+            return View(ViewData);
         }
     }
 }
