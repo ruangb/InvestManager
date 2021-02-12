60818387c278225812097b543d03767f32a72f41
Ruan <ruangbarros@gmail.com>
2021-02-11 22:25:08 -0300

    changing required message and add validation on RentabilityPerMonth view

--- InvestManager/Models/Operation.cs
+++ InvestManager/Models/Operation.cs
@@ -9,35 +9,35 @@ namespace InvestManager.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Column(TypeName = "varchar(20)")]
         [Display(Name = "Ativo")]
         public string Asset { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [DisplayFormat(DataFormatString = "{0:N2}")]
         [Column(TypeName = "decimal(7,2)")]
         [DataType(DataType.Currency)]
         [Display(Name = "Preço")]
         public decimal Price { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Range(0, int.MaxValue, ErrorMessage = "{0} deve ser superior a {1}")]
         [Display(Name = "Quantidade")]
         public int Quantity { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [Display(Name = "Data")]
         public DateTime Date { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Column(TypeName = "varchar(6)")]
         [Display(Name = "Tipo")]
         public string Type { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Column(TypeName ="decimal(1)")]
         public decimal Status { get; set; }
 
@@ -69,13 +69,17 @@ namespace InvestManager.Models
         public IEnumerable<string> Months { get; set; }
 
         [NotMapped]
-        public string ReferenceYear { get; set; }
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Display(Name = "Mês")]
+        public string ReferenceMonth { get; set; }
 
         [NotMapped]
         public IEnumerable<string> Years { get; set; }
 
         [NotMapped]
-        public string ReferenceMonth { get; set; }
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Display(Name = "Ano")]
+        public string ReferenceYear { get; set; }
 
         [NotMapped]
         public IList<Operation> Operations { get; set; }
